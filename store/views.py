13c38f831ae0d231af69a1b4d4b4d19ec388575a
sainger <sultanmanasov6@gmail.com>
2024-06-03 00:48:41 +0600

    Initial commit

---
+++ store/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from .models import Product
+
+
+def product_list(request):
+    products = Product.objects.all()
+    return render(request, 'store/product_list.html', {'products': products})
+
